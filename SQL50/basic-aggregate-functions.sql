4b718b832b81a3d5ab3c71c846a2a46e4f203afb
Juan Cantor <juancantor.all@gmail.com>
2025-08-16 20:26:03 -0500

    sql50-1251

--- SQL50/basic-aggregate-functions.sql
+++ SQL50/basic-aggregate-functions.sql
@@ -2,3 +2,7 @@
 SELECT * from Cinema where MOD(id, 2) <> 0 and description != "boring"
 order by rating desc
 
+-- 1251
+select p.product_id, IFNULL(round(SUM(p.price * u.units)/sum(u.units), 2), 0) as average_price
+from Prices p left join UnitsSold u on p.product_id = u.product_id AND (u.purchase_date between p.start_date and p.end_date)
+group by p.product_id
