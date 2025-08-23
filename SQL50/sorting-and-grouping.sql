cd55e99f103f166858ac9dc752ee41686c495743
Juan Cantor <juancantor.all@gmail.com>
2025-08-23 11:51:14 -0500

    sql50-1141

--- SQL50/sorting-and-grouping.sql
+++ SQL50/sorting-and-grouping.sql
@@ -1,2 +1,7 @@
 --2356
 select t.teacher_id, count(distinct subject_id) as cnt from Teacher t group by t.teacher_id
+
+--1141
+select a.activity_date as day, count(DISTINCT a.user_id) as active_users 
+from Activity a where a.activity_date between "2019-06-28" AND "2019-07-27"
+GROUP BY a.activity_date
