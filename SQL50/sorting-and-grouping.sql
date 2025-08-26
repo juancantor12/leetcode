5a3a2cfa1d4c74854381445e810e0738874f1ebd
Juan Cantor <juancantor.all@gmail.com>
2025-08-26 10:37:25 -0500

    sl150-619

--- SQL50/sorting-and-grouping.sql
+++ SQL50/sorting-and-grouping.sql
@@ -24,3 +24,6 @@ select class from Courses group by class having count(student) >= 5
 
 --1729
 select f.user_id, count(f.follower_id) as followers_count from Followers f group by f.user_id order by f.user_id asc
+
+--619
+select (select num from MyNumbers group by num having count(num) = 1 order by num desc limit 1) as num from dual
