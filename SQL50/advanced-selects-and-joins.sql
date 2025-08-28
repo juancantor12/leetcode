9d62bf7cae25f038cef99352f8fa272c454e1b3c
Juan Cantor <juancantor.all@gmail.com>
2025-08-28 10:48:19 -0500

    sql50-1731

---
+++ SQL50/advanced-selects-and-joins.sql
@@ -0,0 +1,5 @@
+--1731
+select e.employee_id, e.name, count(e2.employee_id) as reports_count, round(avg(e2.age)) as average_age
+from Employees e inner join Employees e2 on e.employee_id = e2.reports_to
+group by e.employee_id, e.name
+order by e.employee_id
