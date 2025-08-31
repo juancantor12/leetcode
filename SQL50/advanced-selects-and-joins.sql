727b61dc7c7f68e9d52a982758e2e35733c5b61c
Juan Cantor <juancantor.all@gmail.com>
2025-08-31 11:31:15 -0500

    sql50-1164

--- SQL50/advanced-selects-and-joins.sql
+++ SQL50/advanced-selects-and-joins.sql
@@ -17,3 +17,12 @@ SELECT x, y, z, IF( (x+y>z AND x+z > y AND y+z>x), "Yes", "No") as triangle from
 select distinct current.num as ConsecutiveNums 
 from Logs previous inner join Logs current on previous.id-1 = current.id inner join Logs next on current.id = next.id+1
 where current.num = previous.num and current.num = next.num
+
+--1164
+select product_id, new_price as price from Products where (product_id,change_date) in (
+    select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id
+    )
+union
+select distinct product_id, 10 as price from Products where product_id not in (
+    select distinct product_id from Products where change_date <='2019-08-16'
+)
