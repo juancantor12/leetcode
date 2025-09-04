90973d4a495d4d466a80f6ec9ae501a5b6d753c4
Juan Cantor <juancantor.all@gmail.com>
2025-09-04 12:03:36 -0500

    sql50-626

--- SQL50/subqueries.sql
+++ SQL50/subqueries.sql
@@ -1,3 +1,6 @@
 --1978
 select employee_id from Employees where salary < 30000 and manager_id not in (select employee_id from Employees)
 order by employee_id
+
+--626
+select if((id = (select max(id) from Seat) and id % 2 = 1) , id, if(id % 2, id+1 , id-1 )) as id, student from Seat order by id
