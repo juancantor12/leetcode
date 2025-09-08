f4fc5c3fee8ac31a5a252266d4114cb261c24f07
Juan Cantor <juancantor.all@gmail.com>
2025-09-08 16:25:23 -0500

    sql50-585

--- SQL50/subqueries.sql
+++ SQL50/subqueries.sql
@@ -33,3 +33,18 @@ select id, count(id) num  from (
     union all
     select accepter_id id from RequestAccepted
 ) as _ group by id order by num desc limit 1
+
+--585
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016 FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
