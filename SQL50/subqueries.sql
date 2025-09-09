e04caac4d9de2636074bbefac35ac187276fa8ff
Juan Cantor <juancantor.all@gmail.com>
2025-09-09 14:33:17 -0500

    sql50-185

--- SQL50/subqueries.sql
+++ SQL50/subqueries.sql
@@ -48,3 +48,12 @@ AND (lat, lon) IN (
     GROUP BY lat, lon
     HAVING COUNT(*) = 1
 )
+
+--185
+select d.name as Department, e.name as Employee, e.salary
+from Employee e join Department d on e.departmentId = d.id
+where(
+        select COUNT(distinct salary)
+        from Employee e2
+        where e2.departmentId = e.departmentId and e2.salary >= e.salary
+    ) <= 3
