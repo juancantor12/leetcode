447e51bdffab397982771216696d3e912baecdbb
Juan Cantor <juancantor.all@gmail.com>
2025-09-11 18:50:58 -0500

    sql50-1527

--- SQL50/advanced-string-functions.sql
+++ SQL50/advanced-string-functions.sql
@@ -3,3 +3,7 @@ select u.user_id, concat(
     upper(left(u.name, 1)), 
     lower(substring(u.name, 2))
 ) as name from Users u order by u.user_id
+
+--1527
+select patient_id, patient_name, conditions from Patients 
+where conditions like "% DIAB1%" or conditions like "DIAB1%" or conditions like "% DIAB1%"
