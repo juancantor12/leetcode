16ff9fb1d33c4a6174030390f6fb641a01ee6da0
Juan Cantor <juancantor.all@gmail.com>
2025-10-11 23:29:19 -0500

    2025-10-10_3147

--- daily-2025/2025-10-10_3147.py
+++ daily-2025/2025-10-10_3147.py
@@ -23,4 +23,7 @@ Example 2:
 """
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
-        
+        last_k = energy[-k:][::-1]
+        for idx in range(len(energy) - k - 1, -1, -1):
+            last_k.append(last_k[-k] + energy[idx])
+        return max(last_k)
