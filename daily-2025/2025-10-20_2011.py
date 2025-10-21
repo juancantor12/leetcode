ffdc5e953616e20ae769be6caac6bcae02d8a918
Juan Cantor <juancantor.all@gmail.com>
2025-10-21 09:57:28 -0500

    2025-10-20_2011

---
+++ daily-2025/2025-10-20_2011.py
@@ -0,0 +1,14 @@
+"""
+There is a programming language with only four operations and one variable X:
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations
+"""
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if op == "++X" or op == "X++": x += 1
+            else: x -= 1
+        return x
