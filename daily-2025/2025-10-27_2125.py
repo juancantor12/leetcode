e8816d23a3ff2eaebb040354f17bfc6f3259c13b
Juan Cantor <juancantor.all@gmail.com>
2025-10-27 17:55:18 -0500

    2025-10-[26,27]_[2043,2125]

---
+++ daily-2025/2025-10-27_2125.py
@@ -0,0 +1,20 @@
+"""
+Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
+
+There is one laser beam between any two security devices if both conditions are met:
+
+The two devices are located on two different rows: r1 and r2, where r1 < r2.
+For each row i where r1 < i < r2, there are no security devices in the ith row.
+Laser beams are independent, i.e., one beam does not interfere nor join with another.
+
+Return the total number of laser beams in the bank.
+"""
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        arr, res = [], 0
+        for row in bank:
+            c = row.count("1")
+            if c > 0: arr.append(c)
+        for i in range(1, len(arr)):
+            res += arr[i] * arr[i-1]
+        return res
