566bfcf990d9a38b7a554448a109f2603161bd37
Juan Cantor <juancantor.all@gmail.com>
2025-10-29 17:47:54 -0500

    2025-10-29_3370

---
+++ daily-2025/2025-10-29_3370.py
@@ -0,0 +1,9 @@
+"""
+You are given a positive number n.
+
+Return the smallest number x greater than or equal to n, such that the binary representation of x contains only set bits
+A set bit refers to a bit in the binary representation of a number that has a value of 1
+"""
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        return (2 ** (len(str(bin(n)))-2) ) - 1
