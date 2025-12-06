d470dd84f10e86b72c65a5682b8306fbdc0357e1
Juan Cantor <juancantor.all@gmail.com>
2025-12-06 01:32:59 -0500

    2025-12-05_3578

---
+++ daily-2025/2025-12-05_3578.py
@@ -0,0 +1,42 @@
+"""
+You are given an integer array nums and an integer k. Your task is to partition nums into one or more non-empty contiguous segments such that in each segment, the difference between its maximum and minimum elements is at most k.
+Return the total number of ways to partition nums under this condition.
+Since the answer may be too large, return it modulo 109 + 7.
+Solution from: https://leetcode.com/problems/count-partitions-with-max-min-difference-at-most-k/solutions/7394795/count-partitions-with-max-min-difference-5hwy/?envType=daily-question&envId=2025-12-06
+"""
+class Solution:
+    def countPartitions(self, nums: List[int], k: int) -> int:
+        MOD = 10**9 + 7
+        sorted_window = SortedList()
+        n = len(nums)
+      
+        # dp[i] = number of ways to partition nums[0:i]
+        dp = [1] + [0] * n
+      
+        # prefix_sum[i] = cumulative sum of dp[0] to dp[i]
+        prefix_sum = [1] + [0] * n
+      
+        # Left pointer for sliding window
+        left = 1
+      
+        # Process each position as potential partition end
+        for right in range(1, n + 1):
+            # Add current element to sorted window
+            sorted_window.add(nums[right - 1])
+          
+            # Shrink window while range exceeds k
+            while sorted_window[-1] - sorted_window[0] > k:
+                sorted_window.remove(nums[left - 1])
+                left += 1
+          
+            # Calculate number of valid partitions ending at position 'right'
+            # We can start a new partition from any position in [left-1, right-1]
+            if left >= 2:
+                dp[right] = (prefix_sum[right - 1] - prefix_sum[left - 2] + MOD) % MOD
+            else:
+                dp[right] = prefix_sum[right - 1] % MOD
+          
+            # Update prefix sum array
+            prefix_sum[right] = (prefix_sum[right - 1] + dp[right]) % MOD
+      
+        return dp[n]
