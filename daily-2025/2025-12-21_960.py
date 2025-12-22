01d239b24cb3cb3ebeb5a7c520ea5314af9504b2
Juan Cantor <juancantor.all@gmail.com>
2025-12-21 22:50:03 -0500

    2025-12-21_960

--- daily-2025/2025-12-21_960.py
+++ daily-2025/2025-12-21_960.py
@@ -4,6 +4,13 @@ We may choose any deletion indices, and we delete all the characters in those in
 For example, if we have strs = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions is ["bef", "vyz"].
 Suppose we chose a set of deletion indices answer such that after deletions, the final array has every string (row) in lexicographic order. (i.e., (strs[0][0] <= strs[0][1] <= ... <= strs[0][strs[0].length - 1]), and (strs[1][0] <= strs[1][1] <= ... <= strs[1][strs[1].length - 1]), and so on). Return the minimum possible value of answer.length.
 """
-class Solution:
-    def minDeletionSize(self, strs: List[str]) -> int:
-        
+class Solution(object):
+    def minDeletionSize(self, A):
+        W = len(A[0])
+        dp = [1] * W
+        for i in range(W-2, -1, -1):
+            for j in range(i+1, W):
+                if all(row[i] <= row[j] for row in A):
+                    dp[i] = max(dp[i], 1 + dp[j])
+
+        return W - max(dp)
