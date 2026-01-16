598ca39c6bc572be8f13db18a09d0bc200d4421a
Juan Cantor <juancantor.all@gmail.com>
2026-01-15 22:13:47 -0500

    2026-01-15_2975

---
+++ daily-2026/2026-01-15_2975.py
@@ -0,0 +1,26 @@
+"""
+2026-01-15_2975
+There is a large (m - 1) x (n - 1) rectangular field with corners at (1, 1) and (m, n) containing some horizontal and vertical fences given in arrays hFences and vFences respectively.
+Horizontal fences are from the coordinates (hFences[i], 1) to (hFences[i], n) and vertical fences are from the coordinates (1, vFences[i]) to (m, vFences[i]).
+Return the maximum area of a square field that can be formed by removing some fences (possibly none) or -1 if it is impossible to make a square field.
+Since the answer may be large, return it modulo 109 + 7.
+Note: The field is surrounded by two horizontal fences from the coordinates (1, 1) to (1, n) and (m, 1) to (m, n) and two vertical fences from the coordinates (1, 1) to (m, 1) and (1, n) to (m, n). These fences cannot be removed.
+"""
+class Solution:
+    def get_edges(self, fences: List[int], border: int) -> set:
+        points = sorted([1] + fences + [border])
+        return {
+            points[j] - points[i]
+            for i in range(len(points))
+            for j in range(i + 1, len(points))
+        }
+
+    def maximizeSquareArea(
+        self, m: int, n: int, hFences: List[int], vFences: List[int]
+    ) -> int:
+        MOD = 10**9 + 7
+        h_edges = self.get_edges(hFences, m)
+        v_edges = self.get_edges(vFences, n)
+
+        max_edge = max(h_edges & v_edges, default=0)
+        return (max_edge * max_edge) % MOD if max_edge else -1
